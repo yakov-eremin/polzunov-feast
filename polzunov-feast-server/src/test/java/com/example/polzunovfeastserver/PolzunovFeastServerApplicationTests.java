5c26e7019f971d745b84fd8eb5c8011cbceaa27f
alekseytumbaev <leha.tumbaev@gmail.com>
2022-12-29 20:42:49 +0700

    server initial commit

---
+++ polzunov-feast-server/src/test/java/com/example/polzunovfeastserver/PolzunovFeastServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.polzunovfeastserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolzunovFeastServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
