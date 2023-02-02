b304ba1ff83fa7bc20bb1be4bf4bb9d60fd1267e
alekseytumbaev <leha.tumbaev@gmail.com>
2023-02-02 16:09:54 +0700

    add UserEntity and UserEntityRepository

---
+++ polzunov-feast-server/src/main/java/com/example/polzunovfeastserver/entity/UserEntity.java
@@ -0,0 +1,56 @@
+package com.example.polzunovfeastserver.entity;
+
+import com.example.polzunovfeastserver.constant.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
+public class UserEntity implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private final String username;
+    private final String password;
+    private final String phone;
+    private final String email;
+    private final Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ polzunov-feast-server/src/main/java/com/example/polzunovfeastserver/repository/UserEntityRepository.java
@@ -0,0 +1,13 @@
+package com.example.polzunovfeastserver.repository;
+
+import com.example.polzunovfeastserver.entity.UserEntity;
+import org.springframework.data.repository.Repository;
+
+import java.util.Optional;
+
+public interface UserEntityRepository extends Repository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
+
+    UserEntity save(UserEntity user);
+}
