c309fc254c9104166b5281efdd5b94ae6a5265b7
alekseytumbaev <leha.tumbaev@gmail.com>
2023-03-27 07:03:22 +0700

    fix error handling, now UserErrorHandler handles exceptions occurred in UserController

--- polzunov-feast-server/src/main/java/com/example/polzunovfeastserver/controller/error_handler/UserErrorHandler.java
+++ polzunov-feast-server/src/main/java/com/example/polzunovfeastserver/controller/error_handler/UserErrorHandler.java
@@ -1,12 +1,13 @@
 package com.example.polzunovfeastserver.controller.error_handler;
 
-import com.example.polzunovfeastserver.controller.UserController;
 import com.example.polzunovfeastserver.exception.already_taken.EmailAlreadyTakenException;
 import com.example.polzunovfeastserver.exception.already_taken.PhoneAlreadyTakenException;
 import com.example.polzunovfeastserver.exception.already_taken.UsernameAlreadyTakenException;
 import com.example.polzunovfeastserver.util.MessageProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.openapitools.model.ErrorResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,10 +15,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static org.springframework.http.HttpStatus.*;
-import static org.springframework.http.HttpStatus.CONFLICT;
 
 @Slf4j
-@RestControllerAdvice(assignableTypes = UserController.class)
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class UserErrorHandler {
 
     private final MessageProvider messageProvider;
