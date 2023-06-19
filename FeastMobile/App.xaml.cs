c474ebb7f9d3475de40c1449877c5e692d097131
Егор Часовских <visualjaw3@gmail.com>
2023-06-19 13:01:17 +0700

    Start mobile version of app

---
+++ FeastMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FeastMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
