3444a1c9322c11cbdcbf1f0e983bade535ec9429
alekseytumbaev <leha.tumbaev@gmail.com>
2023-07-20 16:09:26 +0700

    fix: - 409 response when trying to delete event associated with some routes
    doc:
    - add 409 to DELETE /event/{id}
    - add 409 to DELETE /place/{id} when trying to delete place associated with some events

--- polzunov-feast-server/src/main/java/com/example/polzunovfeastserver/event/EventExceptionHandler.java
+++ polzunov-feast-server/src/main/java/com/example/polzunovfeastserver/event/EventExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.example.polzunovfeastserver.event;
 
 import com.example.polzunovfeastserver.event.exception.EventNotFoundException;
+import com.example.polzunovfeastserver.event.util.EventTableKeys;
 import com.example.polzunovfeastserver.place.excepition.PlaceNotFoundException;
+import com.example.polzunovfeastserver.route.node.util.RouteNodeTableKeys;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
 import org.openapitools.model.ErrorResponse;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -18,6 +21,30 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @RestControllerAdvice(basePackages = "com.example.polzunovfeastserver.event")
 @Order(Ordered.HIGHEST_PRECEDENCE) //needed to not fall into global exception handler
 public class EventExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(CONFLICT)
+    public ErrorResponse onDataIntegrityViolationException(DataIntegrityViolationException e) {
+        String message;
+        if (!(e.getCause() instanceof ConstraintViolationException cause)) {
+            message = "Data integrity violation: ";
+            log.warn(message, e);
+            ErrorResponse error = new ErrorResponse();
+            error.setMessage(message + e.getMessage());
+            return error;
+        }
+
+        //Foreign key constraints violation
+        if (cause.getConstraintName().equals(RouteNodeTableKeys.FOREIGN_EVENT)) {
+            message = "Cannot delete place, because there are events associated with it";
+        } else {
+            message = String.format("Constraint '%s' violation: %s", cause.getConstraintName(), cause.getMessage());
+        }
+        log.warn(message, e);
+        ErrorResponse error = new ErrorResponse();
+        error.setMessage(message);
+        return error;
+    }
 
     @ExceptionHandler(EventNotFoundException.class)
     @ResponseStatus(NOT_FOUND)
